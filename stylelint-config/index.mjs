77ea2cceedff9df2074c91c97beb1af8c97d0521
Bob van de Vijver <bob@drenso.nl>
2026-03-23 13:15:23 +0100

    Add default ignore files to stylelint config

--- stylelint-config/index.mjs
+++ stylelint-config/index.mjs
@@ -3,6 +3,11 @@ export default {
     extends: [
         'stylelint-config-standard-scss',
     ],
+    ignoreFiles: [
+        'public/build/**/*',
+        'public/bundles/**/*',
+        'vendor/**/*',
+    ],
     rules: {
         'declaration-empty-line-before': null,
         'scss/dollar-variable-empty-line-before': null,
