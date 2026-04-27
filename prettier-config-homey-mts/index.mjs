6709ae0dbf59b2d858e4fe57f1e4c893cfa97290
Bob van de Vijver <bob@drenso.nl>
2026-04-27 12:03:41 +0200

    Enable bracket same line

--- prettier-config-homey-mts/index.mjs
+++ prettier-config-homey-mts/index.mjs
@@ -1,6 +1,6 @@
 export default {
     "arrowParens": "avoid",
-    "bracketSameLine": false,
+    "bracketSameLine": true,
     "bracketSpacing": true,
     "semi": true,
     "experimentalTernaries": false,
